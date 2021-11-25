402b5069defd92ba1db35c6dfea25ce5c86caa12
odunsii <92687201+odunsii@users.noreply.github.com>
2021-11-25 21:10:30 +0100

    update
    the final update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-const hamburger = document.getElementById('hamburg')
+const mobileNav = document.querySelector('.mobile-nav');
+const burger = document.querySelector('.burger');
+const contactBtn = document.querySelector('.btn');
+console.log(contactBtn);
+
+
+burger.addEventListener('click',()=>{
+    mobileNav.classList.toggle('mobile-nav-active');
+    burger.classList.toggle('toggle');
+}
+);
